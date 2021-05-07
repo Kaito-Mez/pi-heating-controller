e8d33960e4c146b815b8af635245736661ca7efe
James McClure <jameskm721@gmail.com>
2021-05-07 14:16:32 +1000

    Made to work without gpio

--- webappwithoutgpio.py
+++ webappwithoutgpio.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request
 import json
-from gpioController import gpioController
 from time import sleep
 import asyncio
 import random
 
-controller = gpioController()
 app = Flask(__name__)
 print('objects created')
 
@@ -17,18 +15,13 @@ def index():
 @app.route('/getmethod', methods=['GET'])
 def give_GET_data():
     #return str(controller.servo_data['Current'])
-    temp = str(random.randint(0, 90))
-    print(temp)
     # json.dumps({"Target":controller.servo_data["Target"], "Current":temp})
-    controller.update_current()
-    return json.dumps(controller.servo_data)
+    return json.dumps({"Target":0, "Current":0})
 
 @app.route('/postmethod', methods=['POST'])
 def get_POST_data():
     jsdata = json.loads(request.form['servo_data'])
     print(jsdata["Target"])
-    angle = float(jsdata["Target"])
-    controller.change_target(angle)
     return('done')
 
 if __name__ == '__main__':
